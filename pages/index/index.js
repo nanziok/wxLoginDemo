47527515529b3dbf147130dbb2293053e3704401
mifan89 <596880630@qq.com>
2022-05-03 16:02:24 +0800

    end

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,76 +3,86 @@ const app = getApp()
 
 Page({
     data: {
-        //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        isHide: false
+      userInfo: {},
+      encryptedData: "",
+      iv: "",
+      rawData: "",
+      signature: "",
+      hasUserInfo: false,
+      canIUseGetUserProfile: false,
+      jscode: null,
     },
-
-    onLoad: function() {
-        var that = this;
-        // 查看是否授权
-        wx.getSetting({
-            success: function(res) {
-                if (res.authSetting['scope.userInfo']) {
-                    wx.getUserInfo({
-                        success: function(res) {
-                            // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
-                            // 根据自己的需求有其他操作再补充
-                            // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
-                            wx.login({
-                                success: res => {
-                                    // 获取到用户的 code 之后：res.code
-                                    console.log("用户的code:" + res.code);
-                                    // 可以传给后台，再经过解析获取用户的 openid
-                                    // 或者可以直接使用微信的提供的接口直接获取 openid ，方法如下：
-                                    // wx.request({
-                                    //     // 自行补上自己的 APPID 和 SECRET
-                                    //     url: 'https://api.weixin.qq.com/sns/jscode2session?appid=自己的APPID&secret=自己的SECRET&js_code=' + res.code + '&grant_type=authorization_code',
-                                    //     success: res => {
-                                    //         // 获取到用户的 openid
-                                    //         console.log("用户的openid:" + res.data.openid);
-                                    //     }
-                                    // });
-                                }
-                            });
-                        }
-                    });
-                } else {
-                    // 用户没有授权
-                    // 改变 isHide 的值，显示授权页面
-                    that.setData({
-                        isHide: true
-                    });
-                }
-            }
-        });
+    onLoad() {
+      console.log("onload");
+      if (typeof(wx.getUserProfile) == 'function') {
+        console.log("支持getUserProfile")
+        this.setData({
+          canIUseGetUserProfile: true
+        })
+      }
     },
-
-    bindGetUserInfo: function(e) {
-        if (e.detail.userInfo) {
-            //用户按了允许授权按钮
-            var that = this;
-            // 获取到用户的信息了，打印到控制台上看下
-            console.log("用户的信息如下：");
-            console.log(e.detail.userInfo);
-            //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-            that.setData({
-                isHide: false
-            });
-        } else {
-            //用户按了拒绝按钮
-            wx.showModal({
-                title: '警告',
-                content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-                showCancel: false,
-                confirmText: '返回授权',
-                success: function(res) {
-                    // 用户没有授权成功，不需要改变 isHide 的值
-                    if (res.confirm) {
-                        console.log('用户点击了“返回授权”');
-                    }
-                }
-            });
+    getUserProfile(e) {
+      let that = this;
+      console.log("session有效，继续操作");
+      wx.checkSession({
+          success: function(res) {
+              wx.login({
+                  timeout: 2000,
+                  success: function(res){
+                      that.setData({
+                          jscode: res.code
+                      })
+                      console.log(res,'wx.login0')
+                  },
+                  fail: function(res){
+                      console.log("微信登录失败",res);
+                  }
+              })
+          },
+          fail: (res)=>{
+            console.log("check session err");
+              wx.login({
+                  timeout: 2000,
+                  success: function(res){
+                      this.setData({
+                          jscode: res.code
+                      })
+                      console.log(res,'wx.login')
+                  },
+                  fail: function(res){
+                      console.log("微信登录失败",res);
+                  }
+              })
+          }
+        })
+        // return;
+      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+      // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.getUserProfile({
+        desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            iv: res.iv,
+            encryptedData: res.encryptedData, 
+            rawData: res.rawData,
+            signature: res.signature
+          })
+          console.log("getUserProfile-success", res);
+          wx.request({
+            url: 'https://www.daodanjiaoyugj.com/api/login/login',
+            method: 'POST',
+            data: {jscode:this.data.jscode,rawData:this.data.rawData,iv:this.data.iv,signature:this.data.signature,encryptedData:this.data.encryptedData}
+          })
         }
+      })
+    },
+    getUserInfo(e) {
+      // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
     }
-})
+  })
